d2768ebf915af9e63930565c74986cdd4324c77b
Asa Wilson <awilson@elctech.com>
2009-01-05 15:55:23 -0800

    Added: function to see if any of the forms on the page are dirty. Fixed: stoppers now look at all forms on the page to see if there are any dirty forms instead of just the first one. Added: support for dynamically added elements using livequery, by default this is turned on if livequery is found.

--- jquery.dirtyform.js
+++ jquery.dirtyform.js
@@ -1,6 +1,6 @@
 // TODO's 
-// Support dynamicaly added inputs (livequery dependency)
 // send off forms dirty event when a form is dirtied so the page knows its been tarnished.
+// make the stoppers work for anchor links
 
 
 
@@ -10,21 +10,23 @@ if (typeof jQuery == 'undefined') throw("jQuery could not be found.");
   
   $.extend({
     DirtyForm: {
-      debug: true, // print out debug info? works best with firebug.
-      hasFirebug:   "console" in window && "firebug" in window.console,
-      changedClass: 'changed',
-      logger:       function(msg){
-                      if(this.debug){
-                        msg = "DirtyForm: " + msg;
-                        this.hasFirebug ? console.log(msg) : alert(msg);
+      debug         : false, // print out debug info? works best with firebug.
+      hasFirebug    : "console" in window && "firebug" in window.console,
+      changedClass  : 'changed',
+      logger        : function(msg){
+                        if(this.debug){
+                          msg = "DirtyForm: " + msg;
+                          this.hasFirebug ? console.log(msg) : alert(msg);
+                        }
                       }
-                    }
     }
-  });
+  });  
   
+  // will flag a form as dirty if something is changed on the form.
   $.fn.dirty_form = function(){
     var defaults = {
-      changedClass: $.DirtyForm.changedClass
+      changedClass  : $.DirtyForm.changedClass,
+      dynamic       : $.isFunction($.livequery)
     }
     
     var settings = $.extend(defaults, arguments.length != 0 ? arguments[0] : {});
@@ -51,24 +53,39 @@ if (typeof jQuery == 'undefined') throw("jQuery could not be found.");
       }
     }
     
+    
     return this.each(function(){
       form = $(this);
-      var inputs = $("#" + form.attr("id")).find(":input:not(:hidden,:submit,:password)")
-      $.DirtyForm.logger("Storing Data!");
-      inputs.each(function(i){
-        var input = $(this);
-        input.data('initial', input_value(input)).blur(function(){input_checker(input, inputs)})
-      });
+      var inputs = $(':input:not(:hidden,:submit,:password,:button)', form)
+      
+      $.DirtyForm.logger('Storing Data!');
+      
+      if (settings.dynamic) {
+        // use livequery to perform these functions on the new elements added to the form
+        inputs.livequery(function(){
+          $(this).data('initial', input_value($(this)))
+        }).livequery('blur', function(){
+          input_checker($(this), inputs)
+        });
+      } else {
+        inputs.each(function(i){
+          // add to whats there now, but don't worry about what's there in the future
+          var input = $(this);
+          input.data('initial', input_value(input)).blur(function(){input_checker(input, inputs)})
+        });
+      }
     });
   };
   
+  
+  // this is meant for selecting links that will warn about proceeding if there are any dirty forms on the page
   $.fn.dirty_stopper = function(){
     $.DirtyForm.logger("Setting dirty stoppers")
     
     return this.each(function(){
       stopper = $(this);
       stopper.click(function(event){
-        if($("form").data("dirty")) {
+        if($("form").are_dirty()) {
           event.preventDefault();
           var div = $("<div id='dirty_stopper_dialog'/>").appendTo(document.body)
           href = $(this).attr('href')
@@ -91,13 +108,26 @@ if (typeof jQuery == 'undefined') throw("jQuery could not be found.");
     });
   }
   
+  // not chainable
+  // returns false if any of the forms on the page are dirty
+  $.fn.are_dirty = function (){
+    var dirty = false
+    this.each(function(){
+      if($(this).data('dirty')) {
+        dirty = true;
+      }
+    })
+    return dirty
+  }
+  
+  // This is just for testing purposes...
   $.fn.dirty_checker = function(){    
     $.DirtyForm.logger("Setting dirty checkers!")
     
     return this.each(function(){
       checker = $(this);
       checker.click(function(){
-        if($("form").data("dirty")) {
+        if($("form").are_dirty()) {
           alert("Dirty Form!!");
         } else {
           alert("Clean Form ...phew!");
